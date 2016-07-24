39c67bff9e8000761105ee9bfbcb69ba41ad2d19
shenmj053 <shenmj053@163.com>
2016-07-24 12:56:50 +0800

    add cookies

--- rango/views.py
+++ rango/views.py
@@ -4,18 +4,42 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
     
-    context_dict = {}
-    
     category_list = Category.objects.order_by('-likes')[:10]
-    pages = Page.objects.order_by('-views')[:10]
+    page_list = Page.objects.order_by('-views')[:10]
     
-    context_dict['categories'] = category_list
-    context_dict['pages'] = pages
+    context_dict = {'categories': category_list, 'pages': page_list}
     
-    return render(request, 'rango/index.html', context_dict)
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    
+    reset_last_visit_time = False
+    
+    last_visit = request.session.get('last_visit')
+    
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        
+        if (datetime.now() - last_visit_time).seconds > 5:
+            visits = visits + 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+
+    context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
+
 
 def category(request, category_name_slug):
     
@@ -77,6 +101,9 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 def register(request):
+    #if request.session.test_cookie_worked():
+    #print ">>>> TEST COOKIE WORKED!"
+    #request.session.delete_test_cookie()
     
     registered = False
 
@@ -154,4 +181,9 @@ def restricted(request):
 
 
 def about(request):
-    return render(request, 'rango/about.html', {})
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+
+    return render(request, 'rango/about.html', {'visits': count})
